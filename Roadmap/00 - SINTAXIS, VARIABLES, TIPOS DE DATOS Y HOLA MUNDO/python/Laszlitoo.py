0c4adb87b4e0b4a147a1e75fcabaf21ce52da558
LASZLO <150385513+Laszlitoo@users.noreply.github.com>
2024-09-25 12:30:01 -0300

    Laszlitoo.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Laszlitoo.py
@@ -0,0 +1,34 @@
+# El lenguaje que voy a utilizar es Python
+# https://www.python.org/
+# Este es un comentrio en una sola línea
+"""
+Este es un
+comentario
+en varias
+líneas
+"""
+'''
+Este también es un
+comentario en
+varias líneas
+'''
+my_variable = "Laszlitoo1"
+'''
+Por convención y buena práctica,
+usar MAYÚSCULAS para difinir CONSTANTES en Python
+porque Python no tiene manera de difinir CONSTANTES.
+¡NO! cambiar el valor de esta Variable,
+¡ES UNA CONSTANTE!
+'''
+MY_CONSTANT_VARIABLE = "Laszlitoo2"
+#Tipo de dato: 'Integer' solo números ENTEROS, NO decimales.
+my_int = 1, 2, 3, 10, 99, 1337
+#Tipo de dato: 'Float' números DECIMALES.
+my_float = 1.5, 10.53, 475.3
+#Tipo de dato: 'Boolean' puede ser True o False.
+my_boolean0 = False
+my_boolena1 = True
+#Tipo de dato: 'String' únicamente acepta cadenas de textos.
+my_string = "Esto es un String o cadena de texto"
+my_other_string = 'Esto es otro String o cadena de texto'
+print("¡Hola, Python!")
